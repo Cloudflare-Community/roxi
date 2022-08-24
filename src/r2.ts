8ba32bf358c6041d0cd3ba8fc0942949cbb25d6b
helloimalastair <7487746+helloimalastair@users.noreply.github.com>
2022-08-24 22:30:51 +0300

    update path creation

--- src/r2.ts
+++ src/r2.ts
@@ -1,6 +1,6 @@
 import { sanitizeHeaders } from "./headers";
 const getFromR2 = async (R2: R2Bucket, path: string, etag: string) => {
-	const res = await R2.get(`roxi-${path}`);
+	const res = await R2.get(`roxi${path.replaceAll("/-/", "/")}`);
 	if (!res || res.customMetadata.etag !== etag) {
 		return undefined;
 	}
@@ -15,7 +15,7 @@ const getFromR2 = async (R2: R2Bucket, path: string, etag: string) => {
 };
 
 const putToR2 = (R2: R2Bucket, path: string, res: Response) =>
-	R2.put(`roxi-${path}`, res.body, {
+	R2.put(`roxi${path.replaceAll("/-/", "/")}`, res.body, {
 		httpMetadata: { contentType: res.headers.get("content-type") || undefined },
 		customMetadata: { etag: res.headers.get("etag") as string },
 	});
